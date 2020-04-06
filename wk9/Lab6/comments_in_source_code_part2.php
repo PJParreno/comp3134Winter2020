07fe94fe9965402db3be68ed63476275ce7eff3b
PJ Parreno <PatrickJParreno@gmail.com>
2020-04-06 16:10:16 -0400

    Adding wk1-10 labs to repo

---
+++ wk9/Lab6/comments_in_source_code_part2.php
@@ -0,0 +1,21 @@
+<?php
+
+echo '<h1>This is an example of an H1 header tag</h1>'
+# <!--Created by Patrick Parreno  with employee id: 101085299-->
+
+echo '<h2>Email: </h2>'
+# <!--JohnDoe@Hotmail.com --->
+
+echo '<h3>Password: </h3>'
+# <!--123mypassword! --->
+
+echo '<h4>Address: </h4>'
+# <!--17 Rich Area Drive --->
+
+echo '<h5>Credit Card Number: </h5>'
+# <!--234 2345 2341 12313 --->
+
+echo '<h6>Wifes Name: </h6>'
+# <!--Amanda Doe --->
+
+?>
